918b1476a567abb84e1d1944f40785552eb3370c
Setu Bhatt <bansetu1@gmail.com>
2024-05-03 22:31:56 +0530

    displayed data from backend to frontend from tiffin

--- frontend/src/pages/TiffinSer.jsx
+++ frontend/src/pages/TiffinSer.jsx
@@ -12,26 +12,45 @@ import CardTiffin from "../components/CardTiffin";
 import NavBar from "../components/NavBar";
 
 const TiffinSer = () => {
+
   const [tiffins, setTiffins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const image = "https://images.unsplash.com/photo-1608198399988-341f712c3711?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvc3RlbHxlbnwwfHwwfHx8MA%3D%3D"
 
   useEffect(() => {
-    fetch("https://automatic-info-tracker-backend.onrender.com/data/tiffins/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+    const fetchData = async () => {
+      try {
+        const [response1, response2] = await Promise.all([
+          fetch('https://automatic-info-tracker-backend.onrender.com/data/tiffins/'),
+          fetch('https://automatic-info-tracker-backend.onrender.com/service/tiffins/')
+        ]);
+
+        if (!response1.ok || !response2.ok) {
+          throw new Error('Network response was not ok');
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setTiffins(data);
-      })
-      .catch((error) => {
-        console.error(
-          "There has been a problem with your fetch operation:",
-          error
-        );
-      });
+
+        const [data1, data2] = await Promise.all([
+          response1.text(),
+          response2.text()
+        ]);
+
+        const cleanedText1 = data1.replace(/NaN/g, 'null');
+        const cleanedText2 = data2.replace(/NaN/g, 'null');
+
+        const parsedData1 = JSON.parse(cleanedText1);
+        const parsedData2 = JSON.parse(cleanedText2);
+
+
+        setTiffins([...parsedData1, ...parsedData2]);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -42,12 +61,12 @@ const TiffinSer = () => {
         tiffins.map((tiffin) => (
           <CardTiffin
             key={tiffin.id}
-            name={tiffin.Name}
+            name={tiffin.Name || tiffin.name}
             rating={tiffin.Rating}
-            phone={tiffin.Phone}
-            thumbnail={tiffin.Thumbnail}
-            latitude={tiffin.Latitude}
-            longitude={tiffin.Longitude}
+            phone={tiffin.Phone || tiffin.phone}
+            thumbnail={tiffin.Thumbnail || image}
+            latitude={tiffin.Latitude || tiffin.latitude}
+            longitude={tiffin.Longitude || tiffin.longitude}
           />
         ))}
     </>
